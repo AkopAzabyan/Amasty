b6e9a32af3d6185846c679982a83da6e31f51d89
Akop <blrarm25@gmail.com>
2019-07-08 17:13:44 +0300

    ``

---
+++ code/First/Hello/Controller/Hello/Product.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace First\Hello\Controller\Hello;
+
+use Magento\Checkout\Model\Cart;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\App\Request\Http;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Framework\Data\Form\FormKey;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+
+
+class Product extends \Magento\Framework\App\Action\Action
+{
+    /**
+     * @var FormKey
+     */
+    private $formKey;
+    /**
+     * @var Cart
+     */
+    private $cart;
+    /**
+     * @var Http
+     */
+    private $request;
+    /**
+     * @var ProductRepositoryInterface
+     */
+    private $productRepository;
+    /**
+     * @var array
+     */
+    private $data;
+
+    public function __construct(
+        Context $context,
+        FormKey $formKey,
+        Cart $cart,
+        Http $request,
+        ProductRepositoryInterface $productRepository,
+        array $data = [])
+    {
+        parent::__construct($context);
+        $this->formKey = $formKey;
+        $this->cart = $cart;
+        $this->request = $request;
+        $this->productRepository = $productRepository;
+        $this->data = $data;
+    }
+
+    public function execute()
+    {
+
+        $sku = $this->request->getPost('sku');
+        $product = $this->productRepository->get($sku);
+        $productId = $product->getId();
+        $params = array(
+            'form_key' => $this->formKey->getFormKey(),
+            'product' => $productId,
+            'qty' => 1
+        );
+        //$product = $this->product->load($productId);
+        $this->cart->addProduct($product, $params);
+        $this->cart->save();
+
+    }
+}
--- code/First/Hello/Controller/Hello/World.php
+++ code/First/Hello/Controller/Hello/World.php
@@ -1,24 +1,37 @@
 <?php
+
 namespace First\Hello\Controller\Hello;
 
 use Magento\Framework\App\Action\Context;
+use Magento\Catalog\Api\ProductRepositoryInterface;
+use Magento\Catalog\Model\ProductRepository;
+use Magento\Catalog\Model\ResourceModel\Product\CollectionFactory;
+use Magento\Framework\App\Config;
+use Magento\Framework\View\Result\PageFactory;
+
 
 class World extends \Magento\Framework\App\Action\Action
 {
     public $_pageFactory;
 
+
     public function __construct(
         \Magento\Framework\App\Action\Context $context,
         \Magento\Framework\View\Result\PageFactory $pageFactory
-    ) {
+    )
+    {
         $this->_pageFactory = $pageFactory;
 
         return parent::__construct($context);
     }
 
-    public function execute()
+
+    function execute()
     {
         return $this->_pageFactory->create();
     }
+
 }
+
+
 
