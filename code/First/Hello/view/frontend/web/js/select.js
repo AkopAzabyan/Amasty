d50a62f951bfc6a1357695359f622b23ae34ba1b
ayastr <student2@amasty.com>
2019-07-10 17:44:48 +0300

    to make ajax

--- code/First/Hello/view/frontend/web/js/select.js
+++ code/First/Hello/view/frontend/web/js/select.js
@@ -7,12 +7,21 @@ require(["jquery"], function ($) {
                 var sku = $('#sku').val();
                 $.ajax({
                     url: '/firsthello/hello/product',
-                    data: { sku },
+                    data: {sku},
                     type: 'post',
                     dataType: 'text',
                     success: function (data) {
-                      console.log(data);
-                      $('.selectList').html(data);
+                        var val = JSON.parse(data);
+                        /*console.log(data);*/
+                        $('.selectList').show('fast').html(data);
+                        $.each(val, function (index, value) {
+                            var src = '/pub/media/catalog/product' + value.image;
+                            $('.selectList').append('<li><p>' + value.name + '</p></li>' + '<li><p>' + value.sku + '</p></li>' + '<li><p><img src=' + src + '></p></li>');
+                        });
+                       /* $('.selectList li').on('click',function () {
+                            $('#sku').val($(this).children('.skuProduct').text());
+                            $('.selectList').hide('fast');
+                        })*/
                     }
                 });
             }
